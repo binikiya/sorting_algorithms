533eb029d80b58e34fe54058280adc5b436c131a
binikiya <binikiya123@gmail.com>
2021-12-07 04:09:31 -0800

    Update Selection Sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,16 +9,23 @@ void selection_sort(int *array, size_t size)
 {
 unsigned int i, j, min;
 int temp;
+unsigned int swapped = 0;
 for (i = 0; i < size; i++)
 {
 min = i;
+swapped = 0;
 for (j = i + 1; j < size; j++)
 {
 if (array[j] < array[min])
+{
 min = j;
+swapped = 1;
+}
 }
 temp = array[min];
 array[min] = array[i];
 array[i] = temp;
+if (swapped)
+print_array(array, size);
 }
 }
